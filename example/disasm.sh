8c85458eff2d96d22642d79251613f804e3dc71f
texane <texane@dell.fbx.proxad.net>
2011-01-14 10:25:05 -0600

    [add] disasm script

---
+++ example/disasm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+#/home/texane/sat/bin/arm-none-eabi-objdump -marm -Mforce-thumb -EL -b binary -D /tmp/barfoo
+/home/texane/sat/bin/arm-none-eabi-objdump -marm -EL -b binary -D /tmp/barfoo
