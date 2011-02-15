ca668a5a5faf412e45614b2a588f940ff932c3c3
Peter Zotov <whitequark@whitequark.org>
2011-02-15 19:47:57 +0300

    Bind to localhost.

--- src/gdb-server.c
+++ src/gdb-server.c
@@ -13,6 +13,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include "gdb-remote.h"
 #include "stlink-hw.h"
 
@@ -57,7 +58,7 @@ int serve(struct stlink* sl, int port) {
 
 	struct sockaddr_in serv_addr = {0};
 	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv_addr.sin_port = htons(port);
 
 	if(bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
