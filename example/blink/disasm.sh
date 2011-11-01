ee772151c2aca1508b906982ad30944087e8143b
Karl Palsson <karlp@tweak.net.au>
2011-11-01 18:51:14 +0000

    Remove unneeded old files

--- example/blink/disasm.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-#/home/texane/sat/bin/arm-none-eabi-objdump -marm -Mforce-thumb -EL -b binary -D /tmp/barfoo
-/home/texane/sat/bin/arm-none-eabi-objdump -marm -EL -b binary -D /tmp/barfoo
