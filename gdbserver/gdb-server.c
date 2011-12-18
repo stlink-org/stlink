7eb080490ce34e3daf6e0ab10743113acbc5a635
Uwe Bonnes <bon@elektron.ikp.physik.tu-darmstadt.de>
2011-12-18 23:35:20 +0100

    Move reset/load_param in the open functions, as requested by Karl

--- gdbserver/gdb-server.c
+++ gdbserver/gdb-server.c
@@ -160,8 +160,6 @@ int main(int argc, char** argv) {
 		break;
     }
     
-	stlink_reset(sl);
-	stlink_load_device_params(sl);
 	printf("Chip ID is %08x, Core ID is  %08x.\n", sl->chip_id, sl->core_id);
 
 	sl->verbose=0;
