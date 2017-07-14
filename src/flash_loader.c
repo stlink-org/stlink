45b4e64a3c2e50efb70b8662b0d4c46062b6a36d
Anthony Viallard <viallard@syscom-instruments.com>
2017-07-14 16:01:36 +0200

    Add support of STM32L496xx/4A6xx devices

--- src/flash_loader.c
+++ src/flash_loader.c
@@ -305,7 +305,8 @@ int stlink_flash_loader_write_to_sram(stlink_t *sl, stm32_addr_t* addr, size_t* 
         loader_size = sizeof(loader_code_stm32f0);
     } else if ((sl->chip_id == STLINK_CHIPID_STM32_L4) ||
               (sl->chip_id == STLINK_CHIPID_STM32_L43X) ||
-              (sl->chip_id == STLINK_CHIPID_STM32_L46X))
+              (sl->chip_id == STLINK_CHIPID_STM32_L46X) ||
+	       (sl->chip_id == STLINK_CHIPID_STM32_L496X))
       {
         loader_code = loader_code_stm32l4;
         loader_size = sizeof(loader_code_stm32l4);
