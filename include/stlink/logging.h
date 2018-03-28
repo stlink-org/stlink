7ec1fb78baaa3d4bd7b36cc042248e24d5f481fe
Slyshyk Oleksiy <alexSlyshyk@gmail.com>
2018-03-28 11:03:38 +0300

    remove exit() from lib. fix #634 (#696)
    * remove exit() from lib. fix #634
    * remove the UFATAL loglevel completely.

--- include/stlink/logging.h
+++ include/stlink/logging.h
@@ -10,11 +10,10 @@ extern "C" {
 #endif
 
 enum ugly_loglevel {
-	UDEBUG = 90,
-	UINFO  = 50,
-	UWARN  = 30,
-	UERROR = 20,
-	UFATAL = 10
+    UDEBUG = 90,
+    UINFO  = 50,
+    UWARN  = 30,
+    UERROR = 20
 };
 
 int ugly_init(int maximum_threshold);
@@ -33,8 +32,6 @@ int ugly_log(int level, const char *tag, const char *format, ...);
 #define WLOG(...) WLOG_HELPER(__VA_ARGS__, "")
 #define ELOG_HELPER(format, ...)   ugly_log(UERROR, UGLY_LOG_FILE, format, __VA_ARGS__)
 #define ELOG(...) ELOG_HELPER(__VA_ARGS__, "")
-#define fatal_helper(format, ...)  ugly_log(UFATAL, UGLY_LOG_FILE, format, __VA_ARGS__)
-#define fatal(...) fatal_helper(__VA_ARGS__, "")
 
 #ifdef	__cplusplus
 }
