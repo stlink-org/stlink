eedd9389438512bbacc9ff31220f3bba5a044afa
MAYANK KUMAR <54588244+mayank-03@users.noreply.github.com>
2019-10-23 22:29:51 +0530

    applying conditional operator

---
+++ applying conditional operator.cpp
@@ -0,0 +1,9 @@
+/* applying conditional operator */
+#include<stdio.h>
+int main(){
+	int a,b;
+	printf("enter the value of a,b\n");
+	scanf("%d%d",&a,&b);
+	a>b?printf("a is greatest"):printf("b is greatest");
+}
+
