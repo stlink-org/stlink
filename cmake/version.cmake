38c8b8b499f1d9e87db214e9fb2c144e593c8a08
nightwalker-87 <15526941+Nightwalker-87@users.noreply.github.com>
2020-04-08 23:44:51 +0200

    Cleanup
    - Whitespace fixes
    - Removed old appveyor-mingw script
    - travis-lin-mingw.sh -> .gitignore

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -15,78 +15,77 @@ if (GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
         OUTPUT_VARIABLE     PROJECT_VERSION
         RESULT_VARIABLE     GIT_DESCRIBE_RESULT
         ERROR_VARIABLE      GIT_DESCRIBE_ERROR
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
 
-    if(GIT_DESCRIBE_RESULT EQUAL 0)
+    if (GIT_DESCRIBE_RESULT EQUAL 0)
         # If the sources have been changed locally, add -dirty to the version.
         execute_process (
-        COMMAND             "${GIT_EXECUTABLE}" diff --quiet
-        WORKING_DIRECTORY   "${PROJECT_SOURCE_DIR}"
-        RESULT_VARIABLE     res)
+            COMMAND             "${GIT_EXECUTABLE}" diff --quiet
+            WORKING_DIRECTORY   "${PROJECT_SOURCE_DIR}"
+            RESULT_VARIABLE     res
+            )
 
         if (res EQUAL 1)
             set (PROJECT_VERSION "${PROJECT_VERSION}-dirty")
-        endif()
+        endif ()
 
         # strip a leading v off of the version as proceeding code expectes just the version numbering.
         string(REGEX REPLACE "^v" "" PROJECT_VERSION ${PROJECT_VERSION})
 
         string(REGEX REPLACE "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)(-[.0-9A-Za-z-]+)?([+][.0-9A-Za-z-]+)?$"
-            "\\1;\\2;\\3" PROJECT_VERSION_LIST ${PROJECT_VERSION})
+                             "\\1;\\2;\\3" PROJECT_VERSION_LIST ${PROJECT_VERSION})
         list(LENGTH PROJECT_VERSION_LIST len)
-        if(len EQUAL 3)
+        if (len EQUAL 3)
             list(GET PROJECT_VERSION_LIST 0 PROJECT_VERSION_MAJOR)
             list(GET PROJECT_VERSION_LIST 1 PROJECT_VERSION_MINOR)
             list(GET PROJECT_VERSION_LIST 2 PROJECT_VERSION_PATCH)
             set(__detect_version 1)
             set(__version_str "${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}")
-            if(EXISTS "${PROJECT_SOURCE_DIR}/.version")
+            if (EXISTS "${PROJECT_SOURCE_DIR}/.version")
                 file(READ  "${PROJECT_SOURCE_DIR}/.version" __version_file)
-                if(NOT __version_str STREQUAL __version_file)
+                if (NOT __version_str STREQUAL __version_file)
                     message(STATUS "Rewrite ${PROJECT_SOURCE_DIR}/.version with ${__version_str}")
-                endif()
-            else()
+                endif ()
+            else ()
                 file(WRITE "${PROJECT_SOURCE_DIR}/.version" ${__version_str})
-            endif()
-        else()
-            message(STATUS "Fail to extract version's parts from \"${PROJECT_VERSION}\"")
-        endif()
+            endif ()
+        else ()
+            message(STATUS "Fail to extract version parts from \"${PROJECT_VERSION}\"")
+        endif ()
 
     else(GIT_DESCRIBE_RESULT EQUAL 0)
         message(WARNING "git describe failed.")
         message(WARNING "${GIT_DESCRIBE_ERROR}")
     endif(GIT_DESCRIBE_RESULT EQUAL 0)
-else()
+else ()
     message(STATUS "Git or repo not found.")
-endif()
+endif ()
 
-if(NOT __detect_version)
+if (NOT __detect_version)
     message(STATUS "Try to detect version from \"${PROJECT_SOURCE_DIR}/.version\" file.")
-    if(EXISTS ${PROJECT_SOURCE_DIR}/.version)
+    if (EXISTS ${PROJECT_SOURCE_DIR}/.version)
         #  If git is not available (e.g. when building from source package)
         #  we can extract the package version from .version file.
-        file (STRINGS .version PROJECT_VERSION)
+        file(STRINGS .version PROJECT_VERSION)
 
         # TODO create function to extract semver from file or string and check if it is correct instead of copy-pasting
         string(REGEX REPLACE "^(0|[1-9][0-9]*)[.](0|[1-9][0-9]*)[.](0|[1-9][0-9]*)(-[.0-9A-Za-z-]+)?([+][.0-9A-Za-z-]+)?$"
                              "\\1;\\2;\\3" PROJECT_VERSION_LIST ${PROJECT_VERSION})
         list(LENGTH PROJECT_VERSION_LIST len)
-        if(len EQUAL 3)
+        if (len EQUAL 3)
             list(GET PROJECT_VERSION_LIST 0 PROJECT_VERSION_MAJOR)
             list(GET PROJECT_VERSION_LIST 1 PROJECT_VERSION_MINOR)
             list(GET PROJECT_VERSION_LIST 2 PROJECT_VERSION_PATCH)
             set(__detect_version 1)
-        else()
-            message(STATUS "Fail to extract version's parts from \"${PROJECT_VERSION}\"")
-        endif()
-    else()
+        else ()
+            message(STATUS "Fail to extract version parts from \"${PROJECT_VERSION}\"")
+        endif ()
+    else ()
         message(STATUS "File \"${PROJECT_SOURCE_DIR}/.version\" did not exists.")
-    endif()
-endif()
-
-if(NOT __detect_version)
+    endif ()
     message(FATAL_ERROR "Unable to determine project version")
-endif()
+endif ()
 
 message(STATUS "stlink version: ${PROJECT_VERSION}")
 message(STATUS "Major ${PROJECT_VERSION_MAJOR} Minor ${PROJECT_VERSION_MINOR} Patch ${PROJECT_VERSION_PATCH}")
