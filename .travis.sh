977576215e64b9b138fff890f8fbf52d17572e4d
nightwalker-87 <15526941+Nightwalker-87@users.noreply.github.com>
2020-04-16 21:00:30 +0200

    Partial project restructuring
    - Updated travis CI build config
    - Refactoring of cmake build config
    - Alligned coding style for GUI
    - Whitespace cleanup for GUI
    - Moved source code for GUI

--- .travis.sh
+++ .travis.sh
@@ -24,13 +24,12 @@ if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/_install ../../
 	make && make package && cd -
 
-	echo "--> Building Binary..."
-	mkdir -p build/Binary && cd build/Binary
-	cho "-DCMAKE_BUILD_TYPE=Binary -DCMAKE_INSTALL_PREFIX=$PWD/_install"
-	cmake -DCMAKE_BUILD_TYPE=Binary -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install ../../
-	make && make package && cd -
-
-else #("$TRAVIS_OS_NAME" == "osx")
+#	echo "--> Building Binary..."
+#	mkdir -p build/Binary && cd build/Binary
+#	cho "-DCMAKE_BUILD_TYPE=Binary -DCMAKE_INSTALL_PREFIX=$PWD/_install"
+#	cmake -DCMAKE_BUILD_TYPE=Binary -DCMAKE_TOOLCHAIN_FILE=./cmake/linux-mingw32.cmake -DCMAKE_INSTALL_PREFIX=$PWD/_install ../../
+#	make && make package && cd -
+else [ "$TRAVIS_OS_NAME" == "osx" ];
 	brew install libusb
 
 	echo "--> Building Debug..."
