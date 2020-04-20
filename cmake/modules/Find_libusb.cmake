stlink-org/stlink

commit 7640daae4c0ade48a4179b3a7750272471b8c8e5

Fixed compilation error on windows
Extraction of downloaded libusb archive failed due to wrong command 
option formatting since commit a8c1f41f1b1c9dd1314f99a1d42de1008f5a4f74.

cmake/modules/Find_libusb.cmake
@@ -73,7 +73,7 @@ elseif (WIN32)                                  # Windows
 
     if (NOT LIBUSB_FOUND OR EXISTS "/etc/debian_version")
         # Preparations for installing libusb library
-        find_package(_7zip REQUIRED)
+        find_package(7zip REQUIRED)
         set(LIBUSB_WIN_VERSION 1.0.23)          # set libusb version
         set(LIBUSB_WIN_ARCHIVE libusb-${LIBUSB_WIN_VERSION}.7z)
         set(LIBUSB_WIN_ARCHIVE_PATH ${CMAKE_BINARY_DIR}/${LIBUSB_WIN_ARCHIVE})
@@ -100,8 +100,8 @@ elseif (WIN32)                                  # Windows
                 )
         else ()
             execute_process(
-                COMMAND ${ZIP_EXECUTABLE} x -y ${LIBUSB_WIN_ARCHIVE_PATH} -o ${LIBUSB_WIN_OUTPUT_FOLDER}
-                )
+                COMMAND ${ZIP_EXECUTABLE} x -y ${LIBUSB_WIN_ARCHIVE_PATH} -o${LIBUSB_WIN_OUTPUT_FOLDER}
+                ) # <-- Note the absence of a space character following the -o option!
         endif ()
 
         # Find path to libusb library

commit a7568d3cf93c00705b1809186e069fc52210445f

Fixed package name in Find_libusb module
Regression from previous commit 
7640daae4c0ade48a4179b3a7750272471b8c8e5.

cmake/modules/Find_libusb.cmake
@@ -73,7 +73,7 @@ elseif (WIN32)                                  # Windows
 
     if (NOT LIBUSB_FOUND OR EXISTS "/etc/debian_version")
         # Preparations for installing libusb library
-        find_package(7zip REQUIRED)
+        find_package(_7zip REQUIRED)
         set(LIBUSB_WIN_VERSION 1.0.23)          # set libusb version
         set(LIBUSB_WIN_ARCHIVE libusb-${LIBUSB_WIN_VERSION}.7z)
         set(LIBUSB_WIN_ARCHIVE_PATH ${CMAKE_BINARY_DIR}/${LIBUSB_WIN_ARCHIVE})