1a4aae336af6958c13b53abad6b2262b7f76bdd6
Damian Pala <damian.pala@sentisa.com>
2020-08-27 20:59:49 +0200

    [DEV] Added STM32G031 chip.

--- src/chipid.c
+++ src/chipid.c
@@ -522,6 +522,17 @@ static const struct stlink_chipid_params devices[] = {
             .bootrom_size = 0x2000
         },
         {
+            // STM32G031
+            .chip_id = 0x466,
+            .description = "G031 device",
+            .flash_type = STLINK_FLASH_TYPE_G0,
+            .flash_size_reg = 0x1FFF75E0,    // Section 38.2
+            .flash_pagesize = 0x800,         // 2K (sec 3.2)
+            .sram_size = 0x9000,             // 36K (sec 2.3)
+            .bootrom_base = 0x1fff0000,
+            .bootrom_size = 0x7000           // 28K (table 2)
+        },
+        {
             // STM32G071/081 (from RM0444)
             .chip_id = STLINK_CHIPID_STM32_G0X1,
             .description = "G071/G081 device",
