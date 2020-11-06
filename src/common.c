4e58872913acd4bcbef5ac9b63514cb04bba4727
Ant-ON <prozanton@gmail.com>
2020-11-06 16:17:54 +0500

    Add printing st-link version to st-info

--- src/common.c
+++ src/common.c
@@ -1487,7 +1487,7 @@ int stlink_soft_reset(stlink_t *sl, int halt_on_reset) {
 }
 
 int stlink_run(stlink_t *sl) {
-	struct stlink_reg rr;
+    struct stlink_reg rr;
     DLOG("*** stlink_run ***\n");
 
     stlink_read_reg(sl, 16, &rr);
