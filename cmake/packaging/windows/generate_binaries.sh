5ce54824a5ca819a7ff630b880268febb07ff3e3
nightwalker-87 <15526941+Nightwalker-87@users.noreply.github.com>
2021-04-05 21:55:54 +0200

    Updated CI & MinGW64 cross build configuration
    - Moved config for macOS 10.14 to GitHub Actions
       (Note: preparation only, yet inactive)
    - Removed macOS 10.14 builds from Travis CI
    - Removed Linux build config from Travis CI
    - Split 32- & 64-bit cross builds to separate jobs
    - Minor fixes for MinGW64 cross build config

--- cmake/packaging/windows/generate_binaries.sh
+++ cmake/packaging/windows/generate_binaries.sh
@@ -6,23 +6,25 @@
 #sudo apt-get install mingw-w64
 
 # x86_64
-mkdir build-mingw
-cd build-mingw
+mkdir build-mingw-64
+cd build-mingw-64
 cmake -DCMAKE_SYSTEM_NAME=Windows \
       -DTOOLCHAIN_PREFIX=x86_64-w64-mingw32 \
-      -DCMAKE_TOOLCHAIN_FILE=./cmake/modules/set_toolchain.cmake ..
+      -DCMAKE_TOOLCHAIN_FILE=./../cmake/modules/set_toolchain.cmake ..
 make package
 cp dist/*.zip ../build/Release/dist
+make clean
 cd ..
-rm -rf build-mingw
+rm -rf build-mingw-64
 
 # i686
-mkdir build-mingw
-cd build-mingw
+mkdir build-mingw-32
+cd build-mingw-32
 cmake -DCMAKE_SYSTEM_NAME=Windows \
       -DTOOLCHAIN_PREFIX=i686-w64-mingw32 \
-      -DCMAKE_TOOLCHAIN_FILE=./cmake/modules/set_toolchain.cmake ..
+      -DCMAKE_TOOLCHAIN_FILE=./../cmake/modules/set_toolchain.cmake ..
 make package
 cp dist/*.zip ../build/Release/dist
+make clean
 cd ..
-rm -rf build-mingw
+rm -rf build-mingw-32
