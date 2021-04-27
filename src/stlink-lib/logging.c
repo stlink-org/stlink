939998de76a904d82ba8248baacf4fc26d98badc
anton <prozanton@gmail.com>
2021-04-27 19:46:28 +0500

    Fixed security warning

--- src/stlink-lib/logging.c
+++ src/stlink-lib/logging.c
@@ -29,10 +29,10 @@ int ugly_log(int level, const char *tag, const char *format, ...) {
   va_list args;
   va_start(args, format);
   time_t mytt = time(NULL);
-  struct tm *tt;
-  tt = localtime(&mytt);
-  fprintf(stderr, "%d-%02d-%02dT%02d:%02d:%02d ", tt->tm_year + 1900,
-          tt->tm_mon + 1, tt->tm_mday, tt->tm_hour, tt->tm_min, tt->tm_sec);
+  struct tm tt;
+  localtime_r(&mytt, &tt);
+  fprintf(stderr, "%d-%02d-%02dT%02d:%02d:%02d ", tt.tm_year + 1900,
+          tt.tm_mon + 1, tt.tm_mday, tt.tm_hour, tt.tm_min, tt.tm_sec);
 
   switch (level) {
   case UDEBUG:
